f3e87d3081017cbb301874f86e261a8a04586bc7
Nick Gauthier <ngauthier@gmail.com>
2011-05-14 17:04:55 -0400

    initial application

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -1,11 +1,4 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    @new_post = Post.new
-  end
-
-  def create
-    post = Post.create!(params[:post])
-    redirect_to posts_path
   end
 end
--- blog/test/integration/posts_test.rb
+++ blog/test/integration/posts_test.rb
@@ -4,13 +4,5 @@ class PostsTest < ActionDispatch::IntegrationTest
   test 'create a post' do
     visit '/'
     assert_see 'Posts'
-
-    fill_in 'Title', :with => 'Making a sandwich'
-    fill_in 'Body', :with => 'ham and cheese'
-    click_button 'Create Post'
-
-    assert_see 'Posts'
-    assert_see 'Making a sandwich'
-    assert_see 'ham and cheese'
   end
 end
